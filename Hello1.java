dec46923a83852949498d7158e1645780a23ab5b
Bhavesh_git <bhsamani55@gmail.com>
2024-10-07 22:49:53 +0530

    pattern

--- Hello1.java
+++ Hello1.java
@@ -1,7 +1,37 @@
+
+import java.util.Scanner;
+
 public class Hello1 {
 
     public static void main(String[] args) {
-        System.out.println("Pankil");
+        
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a num:");
+
+        int n = sc.nextInt();
+
+        int r =0;
+        int m=n;
+        int rev=0;
+
+        while (n>0){
+
+            r=n%10;
+            rev=rev*10+r;
+            n=n/10;
+            }
+            if (rev==m) {
+                System.out.println("palindrome. ");
+
+            } else {
+                System.out.println("Not palindrome.");
+
+        }
+
+
+
+
 
     }
     
